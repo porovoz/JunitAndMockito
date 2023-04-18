c1e4e2eab81a578da147cf1923eccc9ab19301ef
porovoz <falkon1@list.ru>
2023-04-18 15:08:29 +0300

    initial commit

---
+++ src/main/java/com/bestapp/junitandmockito/JunitAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.junitandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitAndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitAndMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestapp/junitandmockito/JunitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestapp.junitandmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAndMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
